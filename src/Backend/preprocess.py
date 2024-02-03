0cca052897bfa217de645579f0d0f0ad48fd9363
Ephrem <eph21sz@gmail.com>
2024-02-03 20:03:02 +0300

    add backend

--- src/Backend/preprocess.py
+++ src/Backend/preprocess.py
@@ -1,3 +1,15 @@
-def preprocess(data):
+from PIL import Image
+import numpy as np
+import rembg
 
-    return data
+
+def preprocess(image):
+    with Image.open(image) as img:
+        img = img.convert("L")
+        img_array = np.array(img)
+        output_image = rembg.remove(img_array, bgcolor=(255, 255, 255, 0))
+        output_image = Image.fromarray(output_image)
+        output_image = output_image.convert("RGB")
+        output_image = output_image.resize((150, 150))
+
+    return output_image
